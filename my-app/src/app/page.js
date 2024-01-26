c900d1941ed285396a12320b68006670988f8d36
Jahid Khan <jahidkhan12xx@gmail.com>
2024-01-26 19:59:56 +0600

    first commit

---
+++ my-app/src/app/page.js
@@ -0,0 +1,11 @@
+import FacebookChat from "@/compomemts/FacebookChat";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Facebook chatapp</h2>
+      <FacebookChat/>
+    </div>
+  );
+}
---
+++ my-app/src/compomemts/FacebookChat.jsx
@@ -0,0 +1,11 @@
+"use client"
+import { FacebookProvider, CustomChat } from 'react-facebook';
+function FacebookChat() {
+  return (
+     <FacebookProvider appId="1043941203388547" chatSupport>
+    <CustomChat pageId="105095754524175" minimized={false}/>
+  </FacebookProvider>   
+  )
+}
+
+export default FacebookChat
